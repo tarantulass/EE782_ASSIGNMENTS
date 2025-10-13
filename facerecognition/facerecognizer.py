9de5aa324a256873b8efd67d12519eed0ad97d9e
tarantulass <jeetgovind206@gmail.com>
2025-10-13 13:04:01 +0530

    Face recognizer changedgit add .

--- facerecognition/facerecognizer.py
+++ facerecognition/facerecognizer.py
@@ -1,4 +1,5 @@
 from deepface import DeepFace
+# from deepface.basemodels import VGGFace
 import sys, os
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 from utils.logsetup import get_logger
@@ -7,13 +8,22 @@ from utils.logsetup import get_logger
 def facerecognition(image: str, db_dir: str) -> bool:
     logger = get_logger(__name__)
 
-    for file in db_dir:
-        logger.info(f"Comparing with {file}")
-        result = DeepFace.verify(image, file, model_name="Facenet", detector_backend="opencv", enforce_detection=False)
-        logger.info(f"Face recognition result for {image}: {result}")
-        if result["verified"]:
-            return True
-    return False
+    ## for speed up
+    model = DeepFace.build_model('VGG-Face')   
+    # files = [os.path.join(db_dir, f) for f in os.listdir(db_dir) if os.path.isfile(os.path.join(db_dir, f))]
+    # for file in files:
+    #     logger.info(f"Comparing with {file}")
+    #     result = DeepFace.verify(image, file, model_name="Facenet", detector_backend="opencv", enforce_detection=False)
+    #     logger.info(f"Face recognition result for {image}: {result}")
+    #     if result["verified"]:
+    #         return True
+    # return False
+    try:
+      df = DeepFace.find(img_path = image, db_path = db_dir,
+      model_name = 'VGG-Face', model = model, distance_metric = 'cosine')
+      logger.info(df.head())
+    except Exception as e:
+      logger.error(f"Error encountered: {e}")
 
 if __name__ == "__main__":
     facerecognition("images.webp", "./facerecognition/DBface")  
